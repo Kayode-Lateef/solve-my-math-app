601a96af36e46ecf8a23d844cae288c1d62290ca
kayode <caurdez02@gmail.com>
2022-01-04 01:24:39 +0100

    updates to deploy to heroku

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare function on(): void; 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    on();
+  }
 
   ngOnInit(): void {
   }
